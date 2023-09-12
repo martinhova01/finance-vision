39f8cb6fe2b70499bdf79828a5ccbb7405575db4
martin Høva <mrhova46@gmail.com>
2023-09-12 13:29:27 +0200

    #2

--- FinanceVision/src/main/java/core/Account.java
+++ FinanceVision/src/main/java/core/Account.java
@@ -1,24 +1,50 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class Account {
     
-    private double balance;
+    private double startValue;
     private List<Transaction> transtactions;
 
 
-    public Account(double balance) {
-        this.balance = balance;
+    public Account(double startValue) {
+        this.startValue = startValue;
         transtactions = new ArrayList<>();
     }
 
     public double getBalance(){
+        double balance = startValue;
+        for (Transaction t : transtactions){
+
+            if (t instanceof Income){
+                balance += t.getAmount();
+            }
+            else if (t instanceof Expense){
+                balance -= t.getAmount();
+            }
+        }
+
         return balance;
+
+    }
+
+    public void addTransaction(Transaction t){
+        transtactions.add(t);
     }
 
     public List<Transaction> getIncomes(){
-        return transtactions.stream().filter(a -> a instanceof Income).toList();
+        return getTransactions(t -> t instanceof Income);
+    }
 
+    public List<Transaction> getExpenses(){
+        return getTransactions(t -> t instanceof Expense);
+    }
+
+    public List<Transaction> getTransactions(Predicate<Transaction> p){
+        return transtactions.stream().filter(p).toList();
     }
 
     
