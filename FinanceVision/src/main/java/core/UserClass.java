e03f992301c2022ce9ee6ef338c9349305d6439c
Aksel Normann Antonsen <akselnan@stud.ntnu.no>
2023-09-12 13:35:25 +0200

    implemented userclass #3

---
+++ FinanceVision/src/main/java/core/UserClass.java
@@ -0,0 +1,92 @@
+package core;
+
+public class UserClass {
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private Account account;
+
+
+    public UserClass(String username, String password, String fullName, String email, Account account) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.account = account;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+
+    public void setUsername(String username) {
+        if (username.contains(" ")) {  // må adde username already taken også.
+            throw new IllegalArgumentException();
+        }
+        this.username = username;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setPassword(String password) {
+        if (password.length() < 8 || !password.matches("[a-zA-Z0-9!@#\\$%^&*()\\-_=+\\[\\]{}|;:\"',.<>/?~]+")) { //passord må være minst 8 tegn
+            throw new IllegalArgumentException();
+        }
+        this.password = password;
+    }
+
+
+    public String getFullName() {
+        return fullName;
+    }
+
+
+    public void setFullName(String fullName) {
+        if (!fullName.matches("[a-zA-Z] + ' '")) { // fullt navn må inneholde mellomrom og kan kun inneholde bokstaver
+            throw new IllegalArgumentException();
+        }
+        this.fullName = fullName;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    private boolean validSymbols(String stringToCheck) {
+        if (stringToCheck.matches("^[a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+(\\.[a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+)*$")) {
+            return true;
+        }
+    }
+
+    public void setEmail(String email) {
+        if (!email.contains("@") || validSymbols(email.substring(0, email.indexOf("@")))) { //email må inneholde "@", og kan ikke inneholde andre symboler enn de oppgitt.
+            throw new IllegalArgumentException();
+        }
+        this.email = email;
+    }
+
+
+    public Account getAccount() {
+        return account;
+    }
+
+
+    public void setAccount(Account account) {
+        Account account = new account; // må se mer på Account-klassen først
+        this.account = account; 
+    }
+
+    
+
+    
+
+    
+}
