60f82bf4d021bfcd04b5fec8632772d2287cd994
martin Høva <mrhova46@gmail.com>
2023-09-14 13:09:10 +0200

    removed unused imports

--- FinanceVision/src/test/java/fileSaving/FileSavingTest.java
+++ FinanceVision/src/test/java/fileSaving/FileSavingTest.java
@@ -1,7 +1,6 @@
 package fileSaving;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
@@ -14,7 +13,6 @@ import org.junit.jupiter.api.Test;
 import core.Account;
 import core.Expense;
 import core.Income;
-import core.Transaction;
 import core.User;
 
 public class FileSavingTest {
