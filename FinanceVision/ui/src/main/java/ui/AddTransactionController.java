07e3c58e1ea1f2bf3c3bcc599e3063f18164dd65
martin Høva <mrhova46@gmail.com>
2023-09-27 20:07:37 +0200

    created fxml-file and controller #27

---
+++ FinanceVision/ui/src/main/java/ui/AddTransactionController.java
@@ -0,0 +1,86 @@
+package ui;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import core.Expense;
+import core.Income;
+import core.Transaction;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class AddTransactionController extends AbstractController {
+    
+    @FXML
+    private TextField descriptionField, amountField;
+    @FXML
+    private RadioButton incomeRadioButton, expenseRadioButton;
+    @FXML
+    private Button addTransactionButton, backButton;
+    @FXML
+    private ChoiceBox<String> categoryList;
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    void updateButton(){
+        
+        if (categoryList.getValue() != null && datePicker.getValue() != null && (incomeRadioButton.isSelected() || expenseRadioButton.isSelected()) && !amountField.getText().equals("") && !descriptionField.getText().equals("")){
+            this.addTransactionButton.setDisable(false);
+        }
+        else{
+            this.addTransactionButton.setDisable(true);
+        }
+    }
+    @FXML
+    void handleRbtnClicked(){
+
+        if (incomeRadioButton.isSelected()){
+            categoryList.getItems().clear();
+            categoryList.getItems().addAll(core.User.defaultIncomeCategories);
+        }
+        else if(expenseRadioButton.isSelected()){
+            categoryList.getItems().clear();
+            categoryList.getItems().addAll(core.User.defaultExpenseCategories);
+
+            //add the additional categories for this user
+        }
+
+    }
+
+    @FXML
+    void handleAddTransactionButton() throws IOException {
+        try{
+            String description = descriptionField.getText();
+            String amountString = amountField.getText();
+            double amount = Double.parseDouble(amountString);
+            String category = this.categoryList.getValue();
+            LocalDateTime time = datePicker.getValue().atStartOfDay();
+
+            Transaction t;
+            if (incomeRadioButton.isSelected()) {
+                t = new Income(description, amount, category, time);
+            }
+            else {
+                t = new Expense(description, amount, category, time);
+            }
+            user.getAccount().addTransaction(t);
+
+        }
+        catch(Exception e){
+            notify(e.getLocalizedMessage(), AlertType.WARNING);
+        }
+
+        switchScene("App.fxml", user);
+    }
+
+    @FXML
+    void handleBack() throws IOException{
+        switchScene("App.fxml");
+    }
+}
