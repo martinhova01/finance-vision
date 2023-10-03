56fdb31e37cbb96ad642bf91341143b9d1eaa90b
Petter Teisberg <pteisb@gmail.com>
2023-10-03 13:26:55 +0200

    28 checkstyle core module

--- FinanceVision/core/src/main/java/core/Expense.java
+++ FinanceVision/core/src/main/java/core/Expense.java
@@ -2,13 +2,16 @@ package core;
 
 import java.time.LocalDateTime;
 
+/**
+ * A {@link Transaction} that is defined as an expense.
+ */
 public class Expense extends Transaction {
 
     public Expense(String description, Double amount, String category) {
-        super(description, amount,category);
+        super(description, amount, category);
     }
     
-    public Expense(String description, double amount, String category, LocalDateTime time){
+    public Expense(String description, double amount, String category, LocalDateTime time) {
         super(description, amount, category, time);
     }
 
