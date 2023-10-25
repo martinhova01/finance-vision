8a079551d7a68da3ca1c977872722acf75261d12
martin Høva <mrhova46@gmail.com>
2023-10-25 20:28:44 +0200

    made springboot use Gson, not Jackson #47

--- FinanceVision/springboot/src/main/java/financevision/springboot/restserver/FinanceVisionApplication.java
+++ FinanceVision/springboot/src/main/java/financevision/springboot/restserver/FinanceVisionApplication.java
@@ -1,20 +1,24 @@
 package financevision.springboot.restserver;
 
-import java.util.EnumSet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+
+import com.google.gson.Gson;
+
+import filesaving.JsonFileSaving;
 
 /**
  * The Spring application.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 public class FinanceVisionApplication {
 
-  // @Bean
-  // public Module objectMapperModule() {
-  //   return TodoPersistence.createJacksonModule(EnumSet.of(TodoModelParts.LISTS));
-  // }
+  @Bean
+  public Gson gson() {
+    return new JsonFileSaving().createGson();
+}
 
   public static void main(String[] args) {
     SpringApplication.run(FinanceVisionApplication.class, args);
