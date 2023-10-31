c592f10920d0b0375759524bfb52929e04fba71a
martin Høva <mrhova46@gmail.com>
2023-10-31 12:48:44 +0100

    checkstyle #49

--- FinanceVision/ui/src/main/java/ui/App.java
+++ FinanceVision/ui/src/main/java/ui/App.java
@@ -18,15 +18,15 @@ public class App extends Application {
     /**
      * Helper method for headless testing.
      */
-  public static void supportHeadless() {
-    if (Boolean.getBoolean("headless")) {
-        System.setProperty("testfx.robot", "glass");
-        System.setProperty("testfx.headless", "true");
-        System.setProperty("prism.order", "sw");
-        System.setProperty("prism.text", "t2k");
-        System.setProperty("java.awt.headless", "true");
+    public static void supportHeadless() {
+        if (Boolean.getBoolean("headless")) {
+            System.setProperty("testfx.robot", "glass");
+            System.setProperty("testfx.headless", "true");
+            System.setProperty("prism.order", "sw");
+            System.setProperty("prism.text", "t2k");
+            System.setProperty("java.awt.headless", "true");
+        }
     }
-}
 
 
     @Override
